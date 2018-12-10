6f082ff8b4b4dfd8c75961213d3337e253cc4ec0
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-10 11:37:10 -0500

    Setup drizzle (broken) Copy built contracts since RN has symlink issues

---
+++ app/Core.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Drizzle, generateStore } from "drizzle";
+import MyStringStore from "./contracts/MyStringStore.json";
+import App from "./App";
+
+const options = { contracts: [MyStringStore] };
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+const Core = () => <App drizzle={drizzle} />;
+
+export default Core;
