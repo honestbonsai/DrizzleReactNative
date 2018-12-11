49a1b4caa2c165aa088815f0732c8ef14cee670a
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-11 14:53:49 -0500

    Use our own URL class to shim global.URL since whatwg-url doesn't work on node

--- app/shims.js
+++ app/shims.js
@@ -1,4 +1,10 @@
 import "node-libs-react-native/globals";
 import { btoa } from "Base64";
+import nodeUrl from 'url';
 
 global.btoa = btoa;
+global.URL = class URL {
+    constructor(url) {
+        return nodeUrl.parse(url)
+    }
+}
