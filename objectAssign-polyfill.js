f318fd149048ee2f8e3ac991ee8746a5143d13ee
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-13 11:46:11 -0500

    Clarify comments

--- objectAssign-polyfill.js
+++ objectAssign-polyfill.js
@@ -1,7 +1,8 @@
 /**
  * From https://github.com/facebook/react-native/blob/1151c096dab17e5d9a6ac05b61aacecd4305f3db/Libraries/polyfills/Object.es6.js
  * This on RN's master branch as of Sep 11, 2018, however it has not made it into a release yet.
- * The only modification made was to remove the check for an existing implementation.
+ * 
+ * The only modification made in Truffle's polyfill was to remove the check for an existing implementation.
  * RN 0.57.7 (and below I assume) uses the non-spec compliant Object.assign that breaks in dev RN builds
  * https://github.com/facebook/react-native/issues/16814
  */
