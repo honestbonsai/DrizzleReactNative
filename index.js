1034cf005b713a6581a83bd6f0548d10a46b4a72
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-13 13:41:23 -0500

    Refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
-import "./shims";
+import "./app/shims";
 import { AppRegistry } from "react-native";
-import Core from "./Core";
-import { name as appName } from "./app.json";
+import Core from "./app/Core";
+import { name as appName } from "./app/app.json";
 
 AppRegistry.registerComponent(appName, () => Core);
