2947c05d82083a6537fd1cdbf5d0b8772455703c
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-13 14:22:45 -0500

    Add comments back to ReadString

--- app/screen/ReadString.js
+++ app/screen/ReadString.js
@@ -7,14 +7,22 @@ class ReadString extends React.Component {
     componentDidMount() {
         const { drizzle } = this.props;
         const contract = drizzle.contracts.MyStringStore;
+
+        // let drizzle know we want to watch the `myString` method
         const dataKey = contract.methods["myString"].cacheCall();
+
+        // save the `dataKey` to local component state for later reference
         this.setState({ dataKey });
     }
 
     render() {
+        // get the contract state from drizzleState
         const { MyStringStore } = this.props.drizzleState.contracts;
+
+        // using the saved `dataKey`, get the variable we're interested in
         const myString = MyStringStore.myString[this.state.dataKey];
 
+        // if it exists, then we display its value
         return <Text>My stored string: {myString && myString.value}</Text>;
     }
 }
