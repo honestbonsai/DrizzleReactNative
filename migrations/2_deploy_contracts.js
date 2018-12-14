0d2804ae3e6f711cb883a6d7a1790d0d13277e78
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-14 18:00:40 -0500

    Fix ComplexStorage not being deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const MyStringStore = artifacts.require("MyStringStore");
 const TutorialToken = artifacts.require("TutorialToken.sol");
+const ComplexStorage = artifacts.require("ComplexStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
   deployer.deploy(TutorialToken);
+  deployer.deploy(ComplexStorage);
 };
