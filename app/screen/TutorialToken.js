9c7fb2e3bfc64ad759c9e1a39448812c7a750609
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-14 17:07:30 -0500

    Make a send token example

--- app/screen/TutorialToken.js
+++ app/screen/TutorialToken.js
@@ -2,7 +2,12 @@ import React from "react";
 import { Text, View, Button, TextInput } from "react-native";
 
 class TutorialToken extends React.Component {
-  state = { dataKeyTotalSupply: null, dataKeyAcc0: null, dataKeyAcc1: null };
+  state = {
+    dataKeyTotalSupply: null,
+    dataKeyAcc0: null,
+    dataKeyAcc1: null,
+    numTokens: "0"
+  };
 
   componentDidMount() {
     const { drizzle, drizzleState } = this.props;
@@ -20,6 +25,32 @@ class TutorialToken extends React.Component {
     // save the `dataKey` to local component state for later reference
     this.setState({ dataKeyTotalSupply, dataKeyAcc0, dataKeyAcc1 });
   }
+
+  submit = () => {
+    const { drizzle, drizzleState } = this.props;
+    const { numTokens } = this.state;
+    const contract = drizzle.contracts.TutorialToken;
+    const stackId = contract.methods["transfer"].cacheSend(
+      drizzleState.accounts[1],
+      Number.parseInt(numTokens),
+      { from: drizzleState.accounts[0] }
+    );
+    this.setState({ stackId });
+  };
+
+  getTxStatus = () => {
+    // get the transaction states from the drizzle state
+    const { transactions, transactionStack } = this.props.drizzleState;
+
+    // get the transaction hash using our saved `stackId`
+    const txHash = transactionStack[this.state.stackId];
+
+    // if transaction hash does not exist, don't display anything
+    if (!txHash) return null;
+
+    // otherwise, return the transaction status
+    return `Transaction status: ${transactions[txHash].status}`;
+  };
 
   render() {
     const { drizzleState } = this.props;
@@ -41,7 +72,17 @@ class TutorialToken extends React.Component {
         <Text>
           {drizzleState.accounts[1]}: {balAcc1 && balAcc1.value}
         </Text>
+
+        <Text>Send tokens</Text>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={numTokens => this.setState({ numTokens })}
+          value={this.state.numTokens}
+          placeholder="Enter a number"
+          keyboardType="numeric"
+        />
         <Button title="Submit" onPress={this.submit} />
+        <Text>{this.getTxStatus()}</Text>
       </View>
     );
   }
