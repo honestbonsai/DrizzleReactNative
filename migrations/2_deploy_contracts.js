ec9929c083e83cd6d2e44a2595b31fe4ffe3fd69
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-14 16:15:46 -0500

    Add TutorialToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MyStringStore = artifacts.require("MyStringStore");
+const TutorialToken = artifacts.require("TutorialToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
+  deployer.deploy(TutorialToken);
 };
