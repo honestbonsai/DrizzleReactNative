f576832de76963dde340e867fed1ae054e8ce0dc
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-17 14:44:44 -0500

    Read from dynamically addded contract

--- app/examples/DynamicContract.js
+++ app/examples/DynamicContract.js
@@ -3,10 +3,40 @@ import { Text, View, Button } from "react-native";
 import abi from "./dynamicContractAbi";
 
 // Fill in your own address for the contract you want to add
-const address = "xxx";
+const address = "0x7726f3f5f437dc75462d762cf58d42a5d8e3c8d4";
 const contractName = "DynamicContract";
 
 class DynamicContract extends React.Component {
+  state = {
+    dataKeyTotalSupply: null,
+    dataKeyAcc0: null,
+    dataKeyAcc1: null,
+    numTokens: "0"
+  };
+
+  componentDidMount() {
+    this.setDataKeys();
+  }
+
+  componentDidUpdate() {
+    this.setDataKeys();
+  }
+
+  setDataKeys = () => {
+    const { drizzle, drizzleState } = this.props;
+
+    if (
+      this.state.dataKeyTotalSupply === null &&
+      drizzle.contracts[contractName]
+    ) {
+      const contract = drizzle.contracts[contractName];
+
+      const dataKeyTotalSupply = contract.methods["totalSupply"].cacheCall();
+
+      this.setState({ dataKeyTotalSupply });
+    }
+  };
+
   addContract = () => {
     const { drizzle } = this.props;
     const contractInfo = {
@@ -17,12 +47,20 @@ class DynamicContract extends React.Component {
   };
 
   render() {
-    const { drizzle } = this.props;
+    const { drizzleState } = this.props;
+    const totalSupply = drizzleState.contracts[contractName]
+      ? drizzleState.contracts[contractName].totalSupply[
+          this.state.dataKeyTotalSupply
+        ]
+      : 0;
+
     return (
       <View>
         <Text>Dynamic Contract</Text>
-        {drizzle.contracts[contractName] ? (
-          <Text>{contractName} exists!</Text>
+        {drizzleState.contracts[contractName] ? (
+          <View>
+            <Text>Total Supply: {totalSupply && totalSupply.value}</Text>
+          </View>
         ) : (
           <Button title="Add Contract" onPress={this.addContract} />
         )}
