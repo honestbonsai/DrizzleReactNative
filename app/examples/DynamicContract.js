9789910b77d2d357fe9f12b2d322c28c66cc3088
HL <36248745+honestbonsai@users.noreply.github.com>
2018-12-17 14:23:32 -0500

    Add and wire up btn to add a dynamic contract Use TutorialToken abi

--- app/examples/DynamicContract.js
+++ app/examples/DynamicContract.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import abi from "./dynamicContractAbi";
+
+// Fill in your own address for the contract you want to add
+const address = "xxx";
+const contractName = "DynamicContract";
 
 class DynamicContract extends React.Component {
+  addContract = () => {
+    const { drizzle } = this.props;
+    const contractInfo = {
+      contractName,
+      web3Contract: new drizzle.web3.eth.Contract(abi, address)
+    };
+    drizzle.addContract(contractInfo);
+  };
+
   render() {
+    const { drizzle } = this.props;
     return (
       <View>
         <Text>Dynamic Contract</Text>
+        {drizzle.contracts[contractName] ? (
+          <Text>{contractName} exists!</Text>
+        ) : (
+          <Button title="Add Contract" onPress={this.addContract} />
+        )}
       </View>
     );
   }
